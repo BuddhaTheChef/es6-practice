a5d1addadf479e3c7d4a504b647f4532f8502ae0
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-02-05 13:44:51 -1000

    the map method

--- app.js
+++ app.js
@@ -52,3 +52,74 @@ images.forEach(function(image){
 })
 
 ////////////////////////////////////////////////////////////////////////////////
+//                                map method
+
+var numbers = [1,2,3];
+
+var doubledNumbers = [];
+
+for (var i = 0; i < numbers.length; i++) {
+  doubledNumbers.push(numbers[i] * 2);
+}
+
+var doubled = numbers.map(function(number) {
+  return number * 2;
+})
+
+doubled;
+doubledNumbers;
+
+var cars = [
+  {model: 'Buick', price: 'Cheap'},
+  {model: 'Camaro', price: 'expensive'},
+];
+
+var prices = cars.map(function(car){
+  return car.price;
+});
+
+prices;
+
+//Example 1
+var images = [
+  { height: '34px', width: '39px' },
+  { height: '54px', width: '19px' },
+  { height: '83px', width: '75px' },
+];
+
+
+var heights =[];
+
+heights = images.map(function(image){
+    return image.height;
+})
+
+heights;
+
+
+//Example 2
+var trips = [
+  { distance: 34, time: 10 },
+  { distance: 90, time: 50 },
+  { distance: 59, time: 25 }
+];
+
+var speeds = trips.map(function(trip){
+    return trip.distance / trip.time;
+});
+
+speeds;
+
+
+//Example 3
+var paints = [ { color: 'red' }, { color: 'blue' }, { color: 'yellow' }];
+
+function pluck(array, property) {
+    var colors = array.map(function(color){
+        return color[property]
+    })
+    return colors
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
