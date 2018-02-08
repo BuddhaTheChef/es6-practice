2eba7e09d4529bde7ae1fe9886252a7843a26b53
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-02-08 13:33:28 -1000

    reduce method

--- app.js
+++ app.js
@@ -394,6 +394,47 @@ var inProgress = requests.some(function(request){
 ////////////////////////////////////////////////////////////////////////////////
 //                          Reduce helper method
 
+var numbers = [10,20,30];
+var sum = 0;
+
+for(var i = 0; i < numbers.length; i++) {
+  sum+=numbers[i];
+}
+
+"--"
+
+numbers.reduce(function(sum, number){
+  return sum + number;
+}, 0);
+
+"--"
+
+var primaryColors = [
+  {color: "red" },
+  {color: "yellow" },
+  {color: "blue" }
+];
+
+primaryColors.reduce(function(previous, primaryColor) {
+  previous.push(primaryColor.color)
+
+  return previous;
+}, []);
+
+"---"
+
+function balancedParens(string) {
+  return !string.split('').reduce(function(previous, char) {
+    if(previous < 0) {return previous;}
+    if(char === '(')  {return ++previous;}
+    if(char === ')') {return --previous;}
+    return previous;
+  }, 0);
+}
+
+balancedParens('(((()');
+balancedParens('()');
+balancedParens(')(');
 
 
 ////////////////////////////////////////////////////////////////////////////////
