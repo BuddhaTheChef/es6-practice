eac9bbb3777a45a03045b24f13f47b90232dbbd6
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-02-08 14:17:34 -1000

    reduce helper method

--- app.js
+++ app.js
@@ -436,5 +436,46 @@ balancedParens('(((()');
 balancedParens('()');
 balancedParens(')(');
 
+//Example 1
+var trips = [{ distance: 34 }, { distance: 12 } , { distance: 1 }];
+
+var totalDistance = trips.reduce(function(previous, trip){
+ return previous + trip.distance;
+}, 0);
+
+//Example 2
+var desks = [
+  { type: 'sitting' },
+  { type: 'standing' },
+  { type: 'sitting' },
+  { type: 'sitting' },
+  { type: 'standing' }
+];
+
+var deskTypes = desks.reduce(function(acc, desk) {
+    if(desk.type === 'sitting') {++acc.sitting;}
+    if(desk.type === 'standing') { ++acc.standing;}
+
+    return acc;
+}, { sitting: 0, standing: 0 });
+
+//Example 3
+// For example, given the following array:
+// var numbers = [1, 1, 2, 3, 4, 4];
+// Your function should return
+// [1, 2, 3, 4]
+// Hint: Use the 'reduce' and 'find' helpers.
+
+
+function unique(array) {
+    return array.reduce(function(reducedArray, element) {
+        var duplicate = reducedArray.find(function(reducedArrayElement) {
+            return reducedArrayElement === element;
+        });
+        if (!duplicate) reducedArray.push(element);
+        return reducedArray;
+    }, []);
+}
+
 
 ////////////////////////////////////////////////////////////////////////////////
