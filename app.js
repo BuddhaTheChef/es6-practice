8f7b2d10b4c91bd65354a5703034fc185bc6d655
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-02-09 17:05:09 -1000

    examples for const and let

--- app.js
+++ app.js
@@ -499,3 +499,18 @@ hourlyWage = 45;
 const name = 'Aj';
 let age  = 21;
 const dateOfBirth ='06/25/96';
+
+//Example 2
+const statuses = [
+  { code: 'OK', response: 'Request successful' },
+  { code: 'FAILED', response: 'There was an error with your request' },
+  { code: 'PENDING', response: 'Your reqeust is still pending' }
+];
+let message = '';
+const currentCode = 'OK';
+
+for (var i = 0; i < statuses.length; i++) {
+  if (statuses[i].code === currentCode) {
+    message = statuses[i].response;
+  }
+}
