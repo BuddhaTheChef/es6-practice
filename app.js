d30604dd4921b9b75e552be57dbf045fb07ed5bb
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-02-12 17:57:14 -1000

    default functions

--- app.js
+++ app.js
@@ -666,5 +666,39 @@ const Car = {
   }
 };
 
+////////////////////////////////////////////////////////////////////////////////
+//                      Default function Arguments
+
+
+function makeAjaxRequest(url, method = 'GET') {
+  return method;
+//logic to make request
+
+}
+
+makeAjaxRequest('google.com', null);
+makeAjaxRequest('google.com', 'POST');
+
+
+function User(id) {
+ this.id = id;
+}
+
+function generateId() {
+  return Math.random() * 999999;
+}
+
+
+function createAdminUser(user = new User(generateId())) {
+	user.admin = true;
+
+  return user;
+}
+
+createAdminUser();
+const user = new User(generateId());
+
+
+
 
 ////////////////////////////////////////////////////////////////////////////////
