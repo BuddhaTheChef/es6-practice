bda1420d7eb9f10e97dea5bb2dc91a29c22c5fb7
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-02-12 18:25:24 -1000

    rest operators

--- app.js
+++ app.js
@@ -713,3 +713,57 @@ function addOffset(style = {}) {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+//                             Rest operator
+
+function addNumbers(...numbers) {
+	return numbers.reduce((sum, number) => {
+    return sum + number
+  }, 0);
+}
+
+addNumbers(1,2,3,4,5,6,7,8);
+
+'------------------------'
+
+const defaultColors = ['red','green'];
+const userFavoriteColors = ['orange', 'yellow'];
+const fallColors = ['fire red', 'fall orange'];
+
+defaultColors.concat(userFavoriteColors);
+//or
+['blue',...fallColors, ...defaultColors, ...userFavoriteColors ];
+
+//output : ["blue","fire red","fall orange","red","green","orange","yellow"]
+
+'------------------------'
+
+function validateShoppingList(...items) {
+	if(items.indexOf('milk') < 0) {
+    return ['milk', ...items];
+  }
+  return items;
+}
+
+validateShoppingList('oranges','bread','eggs');
+
+//output : ["milk","oranges","bread","eggs"]
+
+'------------------------'
+
+const MathLibrary = {
+  calculateProduct(...rest) {
+    console.log('please use the multiply method instead');
+    return this.multiply(...rest);
+  },
+  multiply(a,b) {
+    return a * b;
+  }
+};
+
+//Example 1
+
+//Example 2
+
+//Example 3
+
+////////////////////////////////////////////////////////////////////////////////
