a5940ea7802b58499d8ab75336bb2cd083e4dc77
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-02-15 14:15:10 -1000

    generators with trees

--- app.js
+++ app.js
@@ -1126,5 +1126,38 @@ for(let name of engineeringTeam) {
 names;
 
 
+class Comment {
+  constructor(content, children) {
+    this.content = content;
+    this.children = children;
+  }
+  *[Symbol.iterator]() {
+    yield this.content;
+    for(let child of this.children) {
+      yield* child;
+    }
+  }
+}
+
+const children = [
+	new Comment('good comment',[]),
+  new Comment('bad comment',[]),
+  new Comment('meh comment',[])
+];
+
+const tree = new Comment('Great post!', children);
+
+const values = [];
+for(let value of tree) {
+  values.push(value);
+}
+
+values;
+
+//Output:
+4
+["Great post!","good comment","bad comment","meh comment"]
+
+
 
 ////////////////////////////////////////////////////////////////////////////////
