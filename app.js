12e06ce59eb0e5d4bdd40cdd35e1835bc4875c46
Aj Wietecha II <ajwietechaii@yahoo.com>
2018-02-16 15:10:30 -1000

    ES6 Practice completed

--- app.js
+++ app.js
@@ -1175,6 +1175,19 @@ promise.then(() => {
   console.log('BOOOOOooo!!');
 })
 
+//Succses
+url = 'https://jsonplaceholder.typicode.com/posts/';
+
+fetch(url)
+.then(response => response.json())
+.then(data => console.log(data));
+
+//Failed
+url = 'https://jsonplaceholder.typicode123.com/posts123456/';
+
+fetch(url)
+.then(response => console.log(response))
+.catch(error => console.log('BAD', error));
 
 
 
